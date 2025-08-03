5ff2bd8f62d0f12fa61cd154badf5fcceb939da3
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2025-08-03 10:55:09 +0800

    Bugfix: ENV failed to load from prior restructuring

--- src/init/load_env.py
+++ src/init/load_env.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=Path(__file__).resolve().parents[1] / ".env")
+load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env")
