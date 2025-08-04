62f3408206c84effd348a0c9dc23466c2a25920d
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2025-08-04 14:44:05 +0800

    Complete rewrite of clients to account for new approach.  Better separation of responsibilities.

--- src/clients/cmc.py
+++ src/clients/cmc.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from pandas import DataFrame
 
-from src.consts import COL_NAME, COL_SYMBOL, COL_MC, MIN_MEGA_CAP, CMC_API_TOKEN, PATH_DATA_ROOT, COL_PRICE
+from src.consts import COL_NAME, COL_SYMBOL, COL_MC, CMC_API_TOKEN, PATH_DATA_ROOT, COL_PRICE, COL_VOLUME
 
 # Coin Market Cap: https://coinmarketcap.com/api/
 
@@ -10,25 +10,20 @@ BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 DEV_PATH_API_MEGA_CRYPTO = PATH_DATA_ROOT / "api_mega_crypto.pkl"
 
 
-def get_mega_crypto(from_cache=False) -> DataFrame:
+def get_crypto(top: int, from_cache=False) -> DataFrame:
     if from_cache and DEV_PATH_API_MEGA_CRYPTO.exists():
-        return pd.read_pickle(DEV_PATH_API_MEGA_CRYPTO)
-
-    params = {"start": "1", "limit": "1000", "convert": "USD"}
-    headers = {"Accepts": "application/json", "X-CMC_PRO_API_KEY": CMC_API_TOKEN}
-    response = requests.get(BASE_URL, headers=headers, params=params)
-    if response.status_code == 200:
-        df = pd.json_normalize(response.json()["data"])
-        df = _clean_mega(df)
-        return df
+        df = pd.read_pickle(DEV_PATH_API_MEGA_CRYPTO)
     else:
-        print(f"Error fetching data: {response.status_code}")
-        return pd.DataFrame()
+        # sort defaults to "market_cap"; sort_dir defaults to "desc". Specifying anyway for clarity
+        params = {"start": "1", "limit": top, "convert": "USD", "sort": "market_cap", "sort_dir": "desc"}
+        headers = {"Accepts": "application/json", "X-CMC_PRO_API_KEY": CMC_API_TOKEN}
+        response = requests.get(BASE_URL, headers=headers, params=params)
+        response.raise_for_status()
 
+        df = pd.json_normalize(response.json()["data"])
+        df.rename(
+            columns={"quote.USD.market_cap": COL_MC, "quote.USD.price": COL_PRICE, "quote.USD.volume_24h": COL_VOLUME},
+            inplace=True)
+        df.to_pickle(DEV_PATH_API_MEGA_CRYPTO)
 
-def _clean_mega(df: DataFrame) -> DataFrame:
-    df.rename(columns={"quote.USD.market_cap": COL_MC, "quote.USD.price": COL_PRICE}, inplace=True)
-    df = df[[COL_NAME, COL_SYMBOL, COL_MC, COL_PRICE]]
-    df = df[df[COL_MC] > MIN_MEGA_CAP]
-    df.to_pickle(DEV_PATH_API_MEGA_CRYPTO)
-    return df
+    return df[[COL_NAME, COL_SYMBOL, COL_MC, COL_PRICE, COL_VOLUME]]
--- src/clients/fmp.py
+++ src/clients/fmp.py
@@ -2,34 +2,56 @@ import pandas as pd
 import requests
 from pandas import DataFrame
 
-from src.consts import COL_NAME, COL_MC, COL_SYMBOL, MIN_MEGA_CAP, FMP_API_TOKEN, PATH_DATA_ROOT, COL_PRICE
+from src.consts import COL_NAME, COL_MC, COL_SYMBOL, MIN_MEGA_CAP, FMP_API_TOKEN, PATH_DATA_ROOT, COL_PRICE, \
+    MIN_LARGE_CAP, MIN_MID_CAP, MIN_SMALL_CAP, COL_VOLUME, MIN_ULTRA_CAP
 
 # Financial Model Prep: https://intelligence.financialmodelingprep.com/developer/docs/stock-screener-api
 
-BASE_URL = "https://financialmodelingprep.com/api/v3/stock-screener"
 DEV_PATH_API_MEGA_STOCK = PATH_DATA_ROOT / "api_mega_stock.pkl"
-EXCHANGES = ["NYSE", "NASDAQ", "AMEX"]
+
+_BASE_URL = "https://financialmodelingprep.com/api/v3/stock-screener"
+_EXCHANGES = ["NYSE", "NASDAQ", "AMEX"]
+_DEFAULT_PARAM = {"isEtf": False, "isFund": False, "isActivelyTrading": True, "apikey": FMP_API_TOKEN,
+                  "exchange": _EXCHANGES}
 
 
-def get_mega_stock(from_cache=False) -> DataFrame:
+def get_stock(top: int, from_cache=False) -> DataFrame:
+    """
+    Stock-screener can not pre-sort the values. All queries limited to 1000 results.  Results are in no particular
+    order. Need to use market cap limits to restrict return count.
+
+    :param top: How many of the top stocks to return
+    :param from_cache: Pull from cache instead of hitting API
+    :return: DataFrame stripped of unused columns and standardized column names.
+    """
     if from_cache and DEV_PATH_API_MEGA_STOCK.exists():
-        return pd.read_pickle(DEV_PATH_API_MEGA_STOCK)
-
-    params = {"marketCapMoreThan": MIN_MEGA_CAP, "isEtf": False, "isFund": False, "isActivelyTrading": True,
-              "apikey": FMP_API_TOKEN, "exchange": EXCHANGES}
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code == 200:
-        df = pd.DataFrame(response.json())
-        df = _clean(df)
-        return df
+        df = pd.read_pickle(DEV_PATH_API_MEGA_STOCK)
     else:
-        print(f"Error fetching data: {response.status_code}")
-        return pd.DataFrame()
+        params = _DEFAULT_PARAM | {"marketCapMoreThan": _get_cap_restriction(top)}
+        response = requests.get(_BASE_URL, params=params)
+        response.raise_for_status()
+
+        df = pd.DataFrame(response.json())
+        df.rename(columns={"companyName": COL_NAME, "marketCap": COL_MC}, inplace=True)
+        df.to_pickle(DEV_PATH_API_MEGA_STOCK)
+
+    return df[[COL_NAME, COL_SYMBOL, COL_MC, COL_PRICE, COL_VOLUME]]
 
 
-def _clean(df: DataFrame) -> DataFrame:
-    df.rename(columns={"companyName": COL_NAME, "marketCap": COL_MC}, inplace=True)
-    df = df[[COL_NAME, COL_SYMBOL, COL_MC, COL_PRICE]]
-    df = df[df[COL_MC].notnull()]
-    df.to_pickle(DEV_PATH_API_MEGA_STOCK)
-    return df
+def _get_cap_restriction(top: int):
+    """
+    Based on how many of the top stocks are desired, we return an estimate of which cap category that will entail.
+
+    :param top: Top number of stocks desired
+    :return: Estimated market cap that will ensure at least top number of stocks will be returned.
+    """
+    if top <= 5:
+        return MIN_ULTRA_CAP
+    if top <= 50:
+        return MIN_MEGA_CAP
+    if top <= 300:
+        return MIN_LARGE_CAP
+    if top <= 500:
+        return MIN_MID_CAP
+    else:
+        return MIN_SMALL_CAP
