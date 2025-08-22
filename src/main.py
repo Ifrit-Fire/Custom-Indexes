7c268f8f28302a8ce476b59c298e829251545ba8
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2025-08-22 16:48:16 -0400

    Main converted to log. Added TODO

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.clients import fmp, cmc
 from src.config_handler import config
 from src.consts import COL_WEIGHT
 
+# TODO: Test what happens if none, some, and all of the appropriate API keys are provided
 # TODO: Indexes - should always have a latest named file to act as an evergreen link.
 # TODO: Add ability to clean old cache
 # TODO: Can we run main loop in parallel?
@@ -23,11 +24,7 @@ for index, criteria in config.get_all_indexes().items():
     df_refined = dp.refine_data(using=criteria, dfs=[df_stock, df_crypto])
     df_weights = allocations.add_weightings(df_refined, criteria).reset_index(drop=True)
 
-    count = len(df_refined) - len(df_weights)
-    print(f"\tDropped {count} symbols with {len(df_weights)} remaining.")
-    print(f"\tIndex weighted results:")
-    print(df_weights)
-    print(f"\tFinal weighted sum: {df_weights[COL_WEIGHT].sum():.2f}%")
-
     io.save_index(index, df_weights)
-    print()
+    log.info("Phase ends", create=index, max_weight=df_weights[COL_WEIGHT].max(),
+             min_weight=df_weights[COL_WEIGHT].min(), median_weight=df_weights[COL_WEIGHT].median(),
+             count=len(df_weights))
