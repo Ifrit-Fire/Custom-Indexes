1a9fc07ff371a2f9defe89660c5dc52e8f77e936
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2025-09-15 23:10:23 -0400

    Added projection for all stock. All projections now return a copy instead of a panda view.

--- src/data/projection.py
+++ src/data/projection.py
@@ -1,24 +1,54 @@
+from typing import Sequence
+
 import pandas as pd
 
 from src.consts import COL_SYMBOL, COL_NAME, COL_MIC, COL_CIK, COL_FIGI, COL_MC, COL_LIST_DATE, COL_OUT_SHARES, \
-    COL_STATE, COL_POSTAL_CODE, COL_COUNTRY
+    COL_STATE, COL_POSTAL_CODE, COL_COUNTRY, COL_TYPE
+
+
+def view_all_stock(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Projects a DataFrame into the canonical stock list view. Ensures all expected columns exist, fills missing
+    ones with NA, and reorders columns into the standard schema.
+
+    Args:
+        df: Input DataFrame with stock listings (Provider-agnostic).
+
+    Returns:
+        DataFrame with standardized stock listing columns.
+    """
+    columns = [COL_SYMBOL, COL_NAME, COL_TYPE, COL_MIC, COL_CIK, COL_FIGI]
+    return _view(df, columns)
 
 
 def view_symbol_details(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Project a DataFrame into the canonical symbol details view. Ensures all expected columns exist, filling missing
+    Projects a DataFrame into the canonical symbol details view. Ensures all expected columns exist, fills missing
     ones with NA, and reorders columns into the standard schema.
 
     Args:
-        df (pd.DataFrame): Input DataFrame with symbol details (Provider-agnostic).
+        df: Input DataFrame with symbol details (Provider-agnostic).
 
     Returns:
-        pd.DataFrame: DataFrame with standardized symbol details columns.
+        DataFrame with standardized symbol details columns.
     """
     columns = [COL_SYMBOL, COL_NAME, COL_MC, COL_MIC, COL_LIST_DATE, COL_CIK, COL_FIGI, COL_OUT_SHARES, COL_POSTAL_CODE,
                COL_STATE, COL_COUNTRY]
+    return _view(df, columns)
 
+
+def _view(df: pd.DataFrame, columns: Sequence[str]) -> pd.DataFrame:
+    """
+    Returns a projected view of the DataFrame with specified columns. Missing columns are added and filled with pd.NA.
+
+    Args:
+        df: Source DataFrame.
+        columns: Desired column names, in output order.
+
+    Returns:
+        A new copied DataFrame with all specified columns, in order.
+    """
     for col in columns:
         if col not in df.columns:
             df[col] = pd.NA
-    return df[columns]
+    return df[columns].copy()
