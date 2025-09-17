7ffea1d4f49d200782355c09a4edd6613e089a80
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2025-09-16 20:15:18 -0400

    FinnhubProvider now supports fetching all stock

--- src/clients/finnhub.py
+++ src/clients/finnhub.py
@@ -6,40 +6,25 @@ from finnhub import FinnhubAPIException
 
 from src.clients.providerpool import Provider
 from src.consts import API_FINN_TOKEN, COL_SYMBOL, STOCK_TYPES, COL_MC, COL_LIST_DATE, COL_OUT_SHARES, COL_FIGI, \
-    COL_MIC, COL_TYPE, MIC_CODES
+    COL_TYPE, MIC_CODES
 from src.data import processing
 from src.data.source import ProviderSource
 from src.exceptions import APILimitReachedError, NoResultsFoundError
-from src.io import cache
 from src.logger import timber
 
 _CLIENT = finnhub.Client(api_key=API_FINN_TOKEN)
 
 
-def get_all_stock() -> pd.DataFrame:
-    """
-    Retrieve the full list of active U.S. stocks from Finnhub, with caching.
+class FinnhubProvider(Provider):
+    @property
+    def name(self) -> ProviderSource:
+        return ProviderSource.FINNHUB
 
-    Attempts to load from the local API cache if available and valid; otherwise queries the remote API.
-    The results are normalized and cached for future use.
+    def fetch_all_stock(self) -> pd.DataFrame:
+        log = timber.plant()
+        log.info("Phase starts", fetch="stock list", endpoint="finnhub")
 
-    Returns:
-        pd.DataFrame: A DataFrame containing stock data with columns:
-
-            - `COL_FIGI`: Financial Instrument Global Identifier (FIGI).
-            - `COL_MIC`: Market Identifier Code (exchange).
-            - `COL_SYMBOL`: Standardized ticker symbol.
-            - `COL_TYPE`: Security type.
-    """
-    log = timber.plant()
-    log.info("Phase starts", fetch="stock list", endpoint="finnhub")
-    df = cache.load_stock_list(provider="finnhub", filters=MIC_CODES)
-    source = "cache"
-
-    if df.empty:
         base_param = {"exchange": "US"}
-        source = "API"
-
         frames = []
         for mic in MIC_CODES:
             param = base_param | {"mic": mic}
@@ -48,20 +33,14 @@ def get_all_stock() -> pd.DataFrame:
 
         df = pd.concat(frames, ignore_index=True)
         df.rename(columns={"figi": COL_FIGI}, inplace=True)
+        df = df[df[COL_TYPE].isin(STOCK_TYPES)]
         df[COL_SYMBOL] = processing.standardize_symbols(df[COL_SYMBOL])
-        cache.save_stock_list(df=df, provider="finnhub", filters=MIC_CODES)
 
-    df = df[df["type"].isin(STOCK_TYPES)]
-    log.info("Phase ends", fetch="stock list", endpoint="finnhub", count=len(df), source=source)
-    return df[[COL_FIGI, COL_MIC, COL_SYMBOL, COL_TYPE]]
+        log.info("Phase ends", fetch="stock list", endpoint="finnhub", count=len(df), source="API")
+        return df
 
 
-class FinnhubProvider(Provider):
-    @property
-    def name(self) -> ProviderSource:
-        return ProviderSource.FINNHUB
-
-    def fetch(self, symbol: str) -> pd.DataFrame:
+    def fetch_symbol_data(self, symbol: str) -> pd.DataFrame:
         return self._get_company_profile2(symbol)
 
     def _get_company_profile2(self, symbol: str) -> pd.DataFrame:
