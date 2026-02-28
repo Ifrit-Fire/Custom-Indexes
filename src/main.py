964054857edf57208a73cc3ba1997bd051c59052
Ifrit-Fire <7048822+Ifrit-Fire@users.noreply.github.com>
2026-02-28 08:37:24 -0500

    Use stock dataframe in refine_data function

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ for index, criteria in config.get_all_indexes().items():
     log = timber.plant(index)
     log.info("Phase starts", create=index)
 
-    df_refined = processing.refine_data(using=criteria, dfs=[df_crypto])
+    df_refined = processing.refine_data(using=criteria, dfs=[df_market, df_crypto])
     df_weights = allocations.add_weightings(df_refined, criteria).reset_index(drop=True)
 
     io.save_index(index, df_weights)
